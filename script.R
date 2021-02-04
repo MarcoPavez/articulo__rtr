21a2d10a0d9f5b376e3e3fd5f5f19d52b3918931
Marco Pavez <marcopz03@outlook.com>
2021-02-04 15:30:04 -0300

    Update script.R

--- script.R
+++ script.R
@@ -7,9 +7,11 @@ library(sf)
 
 # Importación de bases de datos -------------------------------------------
 
-cnr_nacional <- read_xlsx("C:/Users/marco/Desktop/R/articulo_rtr/base_datos/AR002T0001380.xlsx")
+cnr_nacional <- read_xlsx("C:/Users/marco/Desktop/R/articulo_rtr/base_datos/AR002T0001380.xlsx") # correspondiente a las transferencias por concepto de la ley 18.450
 
-shp_nacional <- read_sf("C:/Users/marco/Desktop/R/articulo_rtr/shp/comunas.shp")
+
+shp_nacional <- read_sf("C:/Users/marco/Desktop/R/articulo_rtr/shp/comunas.shp") # archivo .shp que contiene las coordenadas para el mapeo de las comunas de la región de O'Higgins
+
 
 # Limpieza de bases de datos ----------------------------------------------
 
@@ -17,11 +19,26 @@ cnr_nacional <- cnr_nacional %>% rename(año="Año de llamado" , codigo="Código
                                         cant_beneficiarios="Cantidad de beneficiarios" , region="Región" , provincia="Provincia" , comuna="Comuna" , consultor="Consultor" , 
                                         estrato="Estrato" , origen_fondos="Origen de los Fondos" , tipo_obra="Tipo de Obra" , superficie_fisica="Superficie Física" , 
                                         superficie_tecnificada="Superficie Tecnificada" , estado_proyecto="Estado del Proyecto: Pagado" , fecha_pago_recursos="Fecha pago recursos" , 
-                                        pago_uf="Monto Bonificado UF bono vigente" , costo_total_proyecto_uf= "Costo Total del Proyecto UF bono vigente")
+                                        pago_uf="Monto Bonificado UF bono vigente" , costo_total_proyecto_uf= "Costo Total del Proyecto UF bono vigente") # rename de variables para simplificar su uso
 
 
+cnr_limpio <- cnr_nacional %>% select(año, cant_beneficiarios, region, provincia, comuna, estrato, tipo_obra, 
+                                      superficie_fisica, superficie_tecnificada, fecha_pago_recursos, pago_uf, 
+                                      costo_total_proyecto_uf) # select para elegir las variables que interesan
 
 
+cnr_ohiggins <- cnr_limpio %>% filter(region=="O´Higgins") # filter de las transferencias hacia los productores de la región de O'Higgins
+
+
+shp_nacional <- shp_nacional %>% rename(region="Region", provincia="Provincia", comuna="Comuna") # rename de variables para simplificar su uso
+
+
+shp_ohiggins <- shp_nacional %>% filter(region=="Región del Libertador Bernardo O'Higgins") # filter de las coordenadas de las comunas de la región de O'Higgins
+
+
+# Manipulación de bases de datos ------------------------------------------
+
+pago_cnr_comunal <- cnr_ohiggins %>% group_by(comuna) %>% summarise(pago_uf = sum(pago_uf)) # monto total de las transferencias de la ley 18.450 según comunas de la región de O'Higgins
 
 
 
