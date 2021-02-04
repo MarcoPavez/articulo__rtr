f2982dba064a1c77287845e745172b2b9e8ae2de
Marco Pavez <marcopz03@outlook.com>
2021-02-04 12:10:05 -0300

    script.R
    replicabilidad

---
+++ script.R
@@ -0,0 +1,9 @@
+
+# Librerías ---------------------------------------------------------------
+
+library(tidyverse)
+library(readxl)
+
+# Importación de bases de datos -------------------------------------------
+
+cnr_nacional <- read_xlsx("C:/Users/marco/Desktop/R/articulo_rtr/base_datos/AR002T0001380.xlsx")
